7199186c1d823791c207c87db7c081aafd5021f9
Tymon Zarski <tymon.zarski.stud@pw.edu.pl>
2021-04-24 16:02:28 +0200

    clean master branch

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,12 +1,6 @@
 #include "../include/CarRentalCompany.hpp"
-
+ using namespace std;
 
 int main(){
     CarRentalCompany test = CarRentalCompany();
-
-    time_t now = time(0);
-
-    std::cout<< timeStampToTime(now)<< std::endl;
-
-
 }
