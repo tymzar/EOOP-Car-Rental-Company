992698a65755d8951babcda11d10ee63eff51751
tymzar <zarski.tymon@gmail.com>
2021-05-09 21:22:10 +0200

    create distance calculation method

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,5 +1,6 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
+#define _USE_MATH_DEFINES
 
 #include <stdio.h>
 #include <time.h>
@@ -7,6 +8,7 @@
 #include <string>
 #include <ctime>
 #include <cstring>
+#include <cmath>
 
 enum MANUFACTURER{
     AUDI,
@@ -46,6 +48,8 @@ typedef struct{
 
     std::string timeStampToTime(const time_t rawtime);
     long int timeToTimeStamp(const std::string &str);
-
+    // Employee * findNearestEmployee(double x, double y);
+    double coordinatesDistanceCalculator(double x, double y, double Ex, double Ey);
+    
 
 #endif
--- lib/Utility.cpp
+++ lib/Utility.cpp
@@ -2,6 +2,7 @@
 
 bool validateVINnumber(std::string VINnumber){
 
+        return true;
 }
 
 std::string timeStampToTime(const time_t rawtime)
@@ -29,4 +30,14 @@ long int timeToTimeStamp(const std::string &str)
     timeinfo.tm_sec    = stoi(str.substr(12, 2));
 
     return mktime(&timeinfo);
+}
+
+double coordinatesDistanceCalculator(double x, double y, double Ex, double Ey){
+        const int R = 6371;
+        const double dLat = (y - Ey)*(M_PI / 180);
+        const double dLon = (x - Ex)*(M_PI / 180);
+        const double a = sin(dLat / 2) * sin(dLat / 2) + cos((y)*(M_PI / 180)) * cos((Ey)*(M_PI / 180)) * sin(dLon / 2) * sin(dLon / 2);
+        const double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        const double d = R * c;
+        return ceil(d);
 }
